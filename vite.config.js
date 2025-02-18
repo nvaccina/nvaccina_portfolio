214135c5bf93dc5419093cfcdd8c38b2f3d22c63
nvaccina <niccolovac@gmail.com>
2025-02-18 23:54:56 +0100

    Fix sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import sitemapPlugin from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    sitemapPlugin({
-      hostname: 'https://niccolo-vaccina.netlify.app',
-      generateRobotsTxt: true,
-      robotsTxtOptions: {
-        policies: [
-          { userAgent: '*', allow: '/' },
-        ],
-      },
-      urls: [
-        { url: '/', changefreq: 'daily', priority: 1.0 },
-        { url: '/about', changefreq: 'daily', priority: 0.8 },
-        { url: '/competenze', changefreq: 'daily', priority: 0.8 },
-        { url: '/portfolio', changefreq: 'daily', priority: 0.9 },
-        { url: '/contatti', changefreq: 'daily', priority: 0.7 },
-      ],
-    })
+    vue()    
   ],
 })
