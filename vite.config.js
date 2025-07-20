1dc635fba2d3ebf3d0f24192e8912fb44c9d55fd
nvaccina <niccolovac@gmail.com>
2025-07-20 22:27:10 +0200

    prerendere

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()    
+    vue(),
+    VitePluginPrerender({
+      staticDir: path.resolve(__dirname, 'dist'),
+      routes: ['/', '/chi-sono', '/contatti', '/portfolio', '/competenze'],
+    })
   ],
 })
