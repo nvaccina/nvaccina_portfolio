9419748146adfda1981887eb954489a57cd80e29
nvaccina <niccolovac@gmail.com>
2025-07-20 22:29:59 +0200

    update import

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import { VitePluginPrerender } from 'vite-plugin-prerender'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
