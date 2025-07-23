8ccff6d0d4c36e6198b43327873ffe49e61f6784
nvaccina <niccolovac@gmail.com>
2025-07-23 18:54:56 +0200

    Aggiunto prerendere

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import prerender from 'vite-plugin-prerender'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()    
+    vue(),
+    prerender({
+      routes: ['/', '/portfolio', '/chi-sono', '/competenze', '/contatti']
+    })
   ],
 })
